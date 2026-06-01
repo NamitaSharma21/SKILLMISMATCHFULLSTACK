3110d50fb2ddbd840952f6706f86370a844b9998
NamitaSharma21 <namitasharmams_cse23@its.edu.in>
2026-06-01 23:50:28 +0530

    update backend config and dependencies

--- skillmismatchreact/src/pages/Signup.jsx
+++ skillmismatchreact/src/pages/Signup.jsx
@@ -19,7 +19,7 @@ const Signup = () => {
     }
 
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/signup`,
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/signup`,
         {
           username,
           email,
