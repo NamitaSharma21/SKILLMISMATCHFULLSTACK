8d41b8d41cac4952530a39fb34630031e2a04835
NamitaSharma21 <namitasharmams_cse23@its.edu.in>
2026-06-01 19:16:10 +0530

    prepare backend for deploy

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
@@ -23,11 +23,13 @@ const roadmapRoutes = require('./routes/roadmap');
 app.use('/api', roadmapRoutes);
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/skillmismatchreact")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- skillmismatchreact/src/pages/Login.jsx
+++ skillmismatchreact/src/pages/Login.jsx
@@ -18,10 +18,13 @@ const Login = () => {
     }
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`,
+        {
+          email,
+          password,
+        }
+      );
+
 
       alert(res.data.message);
 
